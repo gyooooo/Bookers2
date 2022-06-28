0683e1ac6723fdf78996e016aa0738603036dd53
gyooooo <harapeko2925@gmail.com>
2022-06-28 15:28:44 +0000

    mama

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :correct_user, only: [:edit, :update, :destroy,]
+  # before_action :correct_user, only: [:edit, :update, :destroy,]
   
    def create
     @book = Book.new(book_params)
